16d4698778b0f0f9f6d69bdfe721b60aca1e2f04
Cody Cogbill <codeman24@gmail.com>
2023-04-25 16:04:53 -0600

    added playlistObj

--- src/index.js
+++ src/index.js
@@ -1,27 +1,36 @@
-const searchTerm = `grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}`
+let playlistObj = {}
+const searchTerm = `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`;
 let authToken = {};
 
-fetch('https://accounts.spotify.com/api/token', {
-    method: 'POST',
-    headers:{
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },    
-    body: searchTerm
+fetch("https://accounts.spotify.com/api/token", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+  body: searchTerm,
 })
-.then((resp) => resp.json())
-.then(obj => {
-    authToken = {...obj}
-})
-.catch(error => console.error(error));
-
+  .then((resp) => resp.json())
+  .then((obj) => {
+    authToken = { ...obj };
+  })
+  .catch((error) => console.error(error));
 
 const getPlaylist = (token, playlistID) => {
-    fetch("https://api.spotify.com/v1/playlists/37i9dQZF1DXcBWIGoYBM5M", {
-        method: "GET",
-        headers: {
-            authorization: `${authToken.token_type} ${authToken.access_token}`
-        } 
-    })
-    .then(resp => resp.json())
-    .then(playlist => console.log(playlist))
-}
+  fetch("https://api.spotify.com/v1/playlists/37i9dQZF1DXcBWIGoYBM5M", {
+    method: "GET",
+    headers: {
+      authorization: `${authToken.token_type} ${authToken.access_token}`,
+    },
+  })
+    .then((resp) => resp.json())
+    .then((playlist) => playlistObj = {...playlist});
+};
+
+
+
+
+
+
+
+
+
