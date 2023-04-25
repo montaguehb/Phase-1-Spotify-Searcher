65757aeb84cdb330d7d85e1dee0b152397c2d08f
Historia Montague <historia.montague@outlook.com>
2023-04-25 13:03:36 -0600

    added persistence to token, and auto refresh

--- src/index.js
+++ src/index.js
@@ -1,27 +1,45 @@
-const searchTerm = `grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}`
-let authToken = {};
+const playlists = [{genre: pop, id: "37i9dQZF1DXcBWIGoYBM5M"}]
+let timer = 3600;
 
-fetch('https://accounts.spotify.com/api/token', {
-    method: 'POST',
-    headers:{
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },    
-    body: searchTerm
-})
-.then((resp) => resp.json())
-.then(obj => {
-    authToken = {...obj}
-})
-.catch(error => console.error(error));
+const getToken = () => {
+    if(localStorage.getItem("expiration") <  Math.floor(Date.now() / 1000)) {
+        localStorage.clear()
+        fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers:{
+            'Content-Type': 'application/x-www-form-urlencoded'
+            },    
+            body: new URLSearchParams({
+                "grant_type": "client_credentials",
+                "client_id": `${clientId}`,
+                "client_secret": `${clientSecret}`
+            }).toString()
+        })
+        .then((resp) => resp.json())
+        .then(obj => {tokenStorage(obj)})
+        .catch(error => console.error(error));
+    }
+    else {
+        timer = localStorage.getItem("expiration") - Math.floor(Date.now() / 1000)
+    }
+}
 
 
 const getPlaylist = (token, playlistID) => {
-    fetch("https://api.spotify.com/v1/playlists/37i9dQZF1DXcBWIGoYBM5M", {
+    fetch(`https://api.spotify.com/v1/playlists/${playlistID}`, {
         method: "GET",
         headers: {
-            authorization: `${authToken.token_type} ${authToken.access_token}`
+            authorization: `${token.token_type} ${token.access_token}`
         } 
     })
     .then(resp => resp.json())
     .then(playlist => console.log(playlist))
 }
+
+const tokenStorage = (tokenObj) => {
+    for (let key in tokenObj) {
+        key === "expires_in"?localStorage.setItem("expiration", `${Math.floor(Date.now() / 1000) + 3600}`):localStorage.setItem(`${key}`, `${tokenObj[key]}`);
+    }
+}
+
+setInterval(getToken(), (timer * 1000))
